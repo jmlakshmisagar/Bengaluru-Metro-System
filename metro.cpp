c6ee1352c29ee8cb28e01bf7b42aef15f6c00344
jmlakshmisagar <jmlakshmisagar@gmail.com>
2024-03-12 23:18:27 +0530

    MetroX-Journey
    Checking shortest, time efficient, user friendly travel.

---
+++ metro.cpp
@@ -0,0 +1,46 @@
+// #####   METRO ------ ROUTER   #####
+
+// -----------LakshmisagarJM@rvce-mca-25'----------------
+
+
+// metro program consists of data structures and alogrithms.
+// minimizing time consumtion and make a better pocket friendly jounrney with optimized code experince
+
+
+
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+
+
+
+
+
+
+
+
+using namespace std;
+
+
+
+void greetingHello(){
+    cout<<"Hello User,   Happy Journeyyyyyy!.......";
+}
+
+
+
+
+
+
+
+
+
+
+int main(){
+    greetingHello();
+
+
+    return 0;
+}
